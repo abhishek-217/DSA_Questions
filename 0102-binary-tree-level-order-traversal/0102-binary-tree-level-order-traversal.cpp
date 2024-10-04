4aa6a876c6e9a3abb0725f4e3044225eb9552f64
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2024-10-04 13:45:59 +0530

    Time: 0 ms (100%), Space: 15 MB (83.69%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,41 +12,42 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root == nullptr){
+        if(root == nullptr){ //Check if root is null then return null
             return {};
         }
         vector<vector<int>>result;
-        queue<TreeNode*>q1; // Create a TreeNode type queue
+        queue<TreeNode*>q1; // Create a TreeNode type queue 
         q1.push(root); //push root element in queue
         q1.push(NULL); //to make level
 
         vector<int>curr; //Make to push in given function type
 
         while(!q1.empty()){
-            TreeNode* temp = q1.front();
-            q1.pop();
+            TreeNode* temp = q1.front();  // Initialize temp to point value at front
+            q1.pop();   //pop the front val
 
             if(temp == NULL){
-                result.push_back(curr);
-                curr.clear();
+                result.push_back(curr); //push to the result matrix
+                curr.clear(); // After takeover the val curr will be clear
+
                 if(!q1.empty()){
-                    q1.push(NULL);
+                    q1.push(NULL); // To change the level
                 }
             }
             else{
-                curr.push_back(temp->val);
+                curr.push_back(temp->val); //Push front val to curr array
                 if(temp->left){
-                    q1.push(temp->left);
+                    q1.push(temp->left); //If not null push the val to childeren node to left
                 }
                 if(temp->right){
-                    q1.push(temp->right);
+                    q1.push(temp->right); //If not null push the val to childeren node to right
                 }
             }
 
         }
 
 
-        return result; 
+        return result;  // return the stored val in the result matrix
         
     }
 };
