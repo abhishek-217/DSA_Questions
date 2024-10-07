a9ea77815224b95024be6f8cd0fde5f4630c84d9
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2024-10-07 11:20:19 +0530

    Time: 12 ms (15.77%), Space: 11.7 MB (31.24%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minLength(string s) {
+        // if(s.size() == 1)
+        stack<char>s1;
+        // stack<char>s2;
+        for(int i=0; i<=s.size()-1; i++){
+
+            if(s1.empty()){ // check stack is empty or not
+
+                s1.push(s[i]);
+            }else{
+                if(s1.top() == 'A' && s[i] == 'B' || s1.top() == 'C' && s[i] == 'D'){
+                    s1.pop(); //if substring match pop or push
+                }else{
+                    s1.push(s[i]);
+                }
+            }
+        }
+        int count =0; // initialize a variable count
+     
+        while(!s1.empty()){
+            count++; //count increase +1 till stack not empty
+            s1.pop();
+        }
+        
+
+        return count; // return number of char in stack;
+        
+    }
+};
