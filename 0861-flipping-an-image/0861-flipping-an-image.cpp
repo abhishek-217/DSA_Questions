6ee6503f474c76f4ebb5e598200941a9acaea134
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2024-10-12 19:24:22 +0530

    Time: 2 ms (76.14%), Space: 11.8 MB (5.2%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.cpp
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -3,31 +3,26 @@ public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
 
         vector<vector<int>>res;
-        vector<vector<int>>ans;
         int l = image.size();
 
-       for(int i = 0; i < l; ++i) {
+       for(int i = 0; i < l; i++) {
             vector<int> re; 
             int j = image[i].size(); 
             while(j > 0) {
                 re.push_back(image[i][j - 1]); // Add elements in reverse order
-                --j;
+                j--;
             }
             res.push_back(re); // Push the reversed row into the result
         }
         
         for(int i=0; i<res.size(); i++){
-            vector<int>an;
-            
+
             for(int j = 0; j<res[i].size(); j++){
-                int s = res[i][j] ^ 1; 
-                an.push_back(s);
-                
+                res[i][j] = res[i][j] ^ 1; 
             }
-            ans.push_back(an);
         }
 
-        return ans;
+        return res;
 
         
     }
