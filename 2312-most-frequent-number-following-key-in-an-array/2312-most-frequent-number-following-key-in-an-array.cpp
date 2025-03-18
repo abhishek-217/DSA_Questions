28cd375c17daa8c32f506f762639eb44dc8bd754
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2025-03-18 23:25:46 +0530

    Time: 2 ms (24.04%), Space: 16.3 MB (65.51%) - LeetHub

---
+++ 2312-most-frequent-number-following-key-in-an-array/2312-most-frequent-number-following-key-in-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        int n=nums.size();
+        unordered_map<int, int>mpp;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==key){
+                mpp[nums[i+1]]++;
+            }
+        }
+        int res=0;
+        int maxi=0;
+        for(auto x:mpp){
+            if(maxi<x.second){
+                maxi=x.second;
+                res=x.first;
+            }
+        }
+        return res;
+    }
+};
