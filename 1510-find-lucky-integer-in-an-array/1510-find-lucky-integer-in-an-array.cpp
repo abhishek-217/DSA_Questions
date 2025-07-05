2eeac8d38456520de83af537219d5d37ba62700b
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2025-07-05 18:54:38 +0530

    Time: 0 ms (100%), Space: 13.9 MB (65.75%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -1,26 +1,57 @@
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
-        int l = arr.size();
+        // int l = arr.size();
 
-        sort(arr.begin(), arr.end());
-        int count =1;
-        int res =-1;
-        for(int i=1; i<=l; i++){
-            if(i<l && arr[i] == arr[i-1]){
-                count++;
+        // sort(arr.begin(), arr.end());
+        // int count =1;
+        // int res =-1;
+        // for(int i=1; i<=l; i++){
+        //     if(i<l && arr[i] == arr[i-1]){
+        //         count++;
 
-            }else{
-                if(arr[i-1] == count){
-                    res = max(res,arr[i-1]);
-                }
-                count =1;
+        //     }else{
+        //         if(arr[i-1] == count){
+        //             res = max(res,arr[i-1]);
+        //         }
+        //         count =1;
 
-            }
+        //     }
 
+        // }
+
+        // return res;
+
+        unordered_map<int,int>freq;
+
+        for(int i: arr){
+            freq[i]++;
         }
 
-        return res;
+        vector<int>ans;
+
+        for(auto &[k,v] : freq){
+            if(k == v){
+                ans.push_back(k);
+            }
+        }
+
+        if(ans.size() == 1){
+            return ans[0];
+        }
+
+        if(ans.size() > 1){
+            int max = ans[0];
+            for(int i=1; i< ans.size(); i++){
+                if(max < ans[i]){
+                    max = ans[i];
+                }
+            }
+
+            return max;
+        }
+
+        return -1;
      
     }
 };
