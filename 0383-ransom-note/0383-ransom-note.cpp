af0865f66c466779575750808535572a15c2829f
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2025-09-03 19:46:18 +0530

    Time: 0 ms (100%), Space: 11.9 MB (51.75%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string ran, string mag) {
+        unordered_map<char,int>mp;
+
+        for(char c:mag){
+            mp[c]++;
+        }
+
+        for(char c: ran){
+            if(mp.find(c) == mp.end()){
+                return false;
+            }else{
+                if(mp[c] == 0){
+                    return false;
+                }
+                mp[c]--;
+            }
+        }
+
+        return true;
+
+    }
+};
