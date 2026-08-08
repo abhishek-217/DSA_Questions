a87c13f3edb4ed8ad55070ac7d37d5cc073e5ec9
Abhishek Kumar <144555996+abhishek-217@users.noreply.github.com>
2026-08-09 05:26:53 +0530

    Time: 4 ms (4.53%), Space: 43.4 MB (8.2%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -4,13 +4,16 @@ class Solution {
 
         for(int i=0; i<s.length(); i++){
 
-            boolean islower = Character.isLowerCase(s.charAt(i));
-            if(!islower){
-                char c = Character.toLowerCase(s.charAt(i));
-                ans += c;
-            }else{
-                ans += s.charAt(i);
-            }
+            // boolean islower = Character.isLowerCase(s.charAt(i));
+            // if(!islower){
+            //     char c = Character.toLowerCase(s.charAt(i));
+            //     ans += c;
+            // }else{
+            //     ans += s.charAt(i);
+            // }
+
+            char c = Character.toLowerCase(s.charAt(i));
+            ans += c;
         }
 
         return ans;
